5b7f6bba9d3c7796a6582e441af0eddbb8011f20
aburrello <alessio.burrello@unibo.it>
2021-05-24 14:52:03 +0200

    first commit in obejct detection

--- layer_custom_generate.py
+++ layer_custom_generate.py
@@ -41,36 +41,57 @@ def main():
     parser.add_argument('--dma_parallelization', default = '8-cores', help = '8-cores or 1-core')
     parser.add_argument('--fc_frequency', default = 100000000, help = 'frequency of fabric controller')
     parser.add_argument('--cl_frequency', default = 100000000, help = 'frequency of cluster')
-    parser.add_argument('--optional', default = 'mixed-sw', help = '8bit, mixed-sw, mixed-hw, 1D_Conv') # change here to change backend kernels
+    parser.add_argument('--optional', default = '8bit', help = '8bit, mixed-sw, mixed-hw, 1D_Conv') # change here to change backend kernels
     args = parser.parse_args()
-
+    precision_dict_act_out     = [8,8,8] # change first number to change BitOut of the layer
+    precision_dict_weights = [8,8,8] # change first number to change BitW of the laye
+    BitIn = 8 # BitIn of the layer
+    
     new_node = node()
     new_node.input_index = 0 # don't touch
     new_node.output_index = 1 # don't touch
-    new_node.padding_top    = 1
-    new_node.padding_left   = 1
-    new_node.padding_bottom = 1
-    new_node.padding_right  = 1
-    new_node.input_h = 16 
-    new_node.input_w = 16
+    new_node.padding_top    = 0
+    new_node.padding_left   = 0
+    new_node.padding_bottom = 0
+    new_node.padding_right  = 0
+    new_node.input_h = 10
+    new_node.input_w = 82
     new_node.output_h = new_node.input_h
     new_node.output_w = new_node.input_w
-    new_node.input_channels = 32
-    new_node.output_channels = 64 # = new_node.input_channels if DW
+    new_node.input_channels = 16
+    new_node.output_channels = 32 # = new_node.input_channels if DW
     new_node.outshift = 3 # don't touch
-    new_node.filter_size_h = 3
-    new_node.filter_size_w = 3
+    new_node.filter_size_h = 1
+    new_node.filter_size_w = 1
     new_node.groups = 1 # put new_node.input_channels if DW
     new_node.stride = 1 # don't touch
     new_node.MACs = new_node.output_h * new_node.output_w * new_node.output_channels * new_node.filter_size_h * new_node.filter_size_w * new_node.input_channels
     new_node.name = 'Conv'
     new_node.weights = np.random.randint(low = -127, high = 128, size = (new_node.input_channels, new_node.filter_size_h, new_node.filter_size_w, new_node.output_channels))
-    precision_dict_act_out     = [2,8] # change first number to change BitOut of the layer
-    precision_dict_weights = [2,8] # change first number to change BitW of the laye
-    BitIn = 2 # BitIn of the layer
+    new_node2 = node()
+    new_node2.input_index = 1 # don't touch
+    new_node2.output_index = 2 # don't touch
+    new_node2.padding_top    = 0
+    new_node2.padding_left   = 0
+    new_node2.padding_bottom = 0
+    new_node2.padding_right  = 0
+    new_node2.input_h = 1
+    new_node2.input_w = 1
+    new_node2.output_h = new_node.input_h
+    new_node2.output_w = new_node.input_w
+    new_node2.input_channels = 32
+    new_node2.output_channels = 96 # = new_node.input_channels if DW
+    new_node2.outshift = 3 # don't touch
+    new_node2.filter_size_h = 1
+    new_node2.filter_size_w = 1
+    new_node2.groups = 1 # put new_node.input_channels if DW
+    new_node2.stride = 1 # don't touch
+    new_node2.MACs = new_node.output_h * new_node.output_w * new_node.output_channels * new_node.filter_size_h * new_node.filter_size_w * new_node.input_channels
+    new_node2.name = 'Gemm'
+    new_node2.weights = np.random.randint(low = -127, high = 128, size = (new_node.input_channels, new_node.filter_size_h, new_node.filter_size_w, new_node.output_channels))
     final_node = node()
-    final_node.input_index = 1
-    final_node.output_index = 2
+    final_node.input_index = 2
+    final_node.output_index = 3
     final_node.padding_top    = 0
     final_node.padding_left   = 0
     final_node.padding_bottom = 0
@@ -90,6 +111,7 @@ def main():
     final_node.weights = np.random.randint(low = -127, high = 128, size = (final_node.input_channels, final_node.filter_size_h, final_node.filter_size_w, final_node.output_channels))
     PULP_Nodes_Graph = []
     PULP_Nodes_Graph.append(new_node)
+    PULP_Nodes_Graph.append(new_node2)
     PULP_Nodes_Graph.append(final_node)
     model_deploy('GAP8', args.chip).print_model_network(PULP_Nodes_Graph,
                             100,
@@ -103,7 +125,7 @@ def main():
                             args.l2_buffer_size,
                             args.fc_frequency,
                             args.cl_frequency,
-                            2, 2, BitIn, args.Bn_Relu_Bits, 
+                            8, 8, BitIn, args.Bn_Relu_Bits, 
                             args.sdk,
                             args.dma_parallelization,args.
                             optional,
